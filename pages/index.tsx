aaa28f59f62b4f699d7c5a78e53614cf908fa483
Godfred Akpan <godfredakpan@gmail.com>
2023-04-22 10:40:50 +0100

    update seo for my website

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,8 +12,29 @@ const Home: NextPage = () => {
         <title>Godfred Akpan - Fullstack Developer</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="author" content="Godfred Akpan" />
-        <meta name="keywords" content="ReactJS, NextJS, NodeJS, JavaScript, AWS, Auth0 " />
-        <meta name="description" content="I am a talented Fullstack developer with a passion for solving real time problems" />
+        <meta name="title" content="Experienced Software Developer | Expertise in Full-Stack Development" />
+        
+        <meta name="keywords" content="Software development, custom software development, software solutions, web development, mobile app development, cloud application development, software testing, software quality assurance, programming languages, software engineering, agile development, software design, software maintenance, software upgrades, software consulting, software project management, software outsourcing, IT services, software support, software integration, software security, software scalability, software architecture, software deployment, Software development,
+        Coding,
+        Programming,
+        Computer science,
+        Technology,
+        Software engineering,
+        Development tools,
+        Debugging,
+        Agile methodology,
+        Software design patterns,
+        Open-source software,
+        Artificial intelligence,
+        Machine learning,
+        Cloud computing,
+        DevOps,
+        Cybersecurity,
+        Data analysis,
+        Quality assurance,
+        Project management,
+        Software testing." />
+        <meta name="description" content="Looking for an experienced software developer with expertise in full-stack development? Look no further! Our developer has a proven track record of designing, developing, and deploying high-quality software solutions across a variety of platforms and industries. With a keen eye for detail and a passion for innovation, our developer is committed to delivering top-notch results that meet the unique needs of your business. Contact us today to learn more about how our software development expertise can benefit your organization." />
         <meta charSet="UTF-8" />
         <meta name="theme-color" content="#2D2E32" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
